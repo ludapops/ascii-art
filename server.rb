89d6160896a8ffc6c2092ef3613b71076ee7afdb
Marc Mathieu <goheat631@gmail.com>
2015-01-24 19:59:15 -0500

    word appears in page source but not on final page

--- server.rb
+++ server.rb
@@ -1,7 +1,10 @@
 require 'pry'
 require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'artii'
 
 
+# enable :sessions
 
 get '/' do 
 	erb :index
@@ -9,5 +12,8 @@ end
 
 get '/ascii/:word' do 
 	@art = params[:word]
+	a = Artii::Base.new :font => 'slant'
+	@final = a.asciify(@art)
+	"#{@final}"
 
 end
